41eff14a2caa8cd089d8f9917fc5f97914319bc5
Tomas Barton <barton.tomas@gmail.com>
2013-09-28 22:40:54 +0200

    updated default param values

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,10 +1,10 @@
 class spark::params {
   
-  $spark_package = hiera('spark_package', 'spark-hadoop1')
-  $spark_version = hiera('spark_version', '0.8.0+SNAPSHOT-f1f92c88')
-  $spark_home    = hiera('spark_home', '/usr/share/spark-hadoop1')
+  $spark_package = hiera('spark_package', 'spark')
+  $spark_version = hiera('spark_version', '0.8.0')
+  $spark_home    = hiera('spark_home', '/usr/share/spark')
   
-  $scala_version = hiera('spark_version', '2.9.2+dfsg-1')
+  $scala_version = hiera('spark_version', '2.9.3-400')
   $scala_home    = hiera('scala_home', '/usr')
   $scala_lib     = hiera('scala_lib', '/usr/share/java') 
   
